03ef90935eda036603711a3f6d9745ac3f805d32
mustafasuhateber <msteber2506@gmail.com>
2022-06-11 22:54:40 -0400

    update

--- app.js
+++ app.js
@@ -2,18 +2,51 @@
 const canvas = document.querySelector("#mainWindow");
 
 class Snake{
+
     xPos;
     yPos;
     size;
-    constructor(x,y,size){
+    direction;
+
+    constructor(x,y,size,direction){
         this.xPos = x;
         this.yPos = y;
         this.size = size;
+        this.direction = direction;
     }
+
     draw(ctx){
-            ctx.fillRect(this.xPos+snake.size,this.yPos,this.size,this.size);
+            ctx.fillRect(this.xPos,this.yPos,this.size,this.size);
         }
     
+    moveRight(unit){
+        if(this.xPos+this.size+unit>canvasWidth){
+            this.xPos = canvasWidth-this.size;
+        }
+        else{this.xPos+=unit;}
+    }
+ 
+    moveLeft(unit){
+        if(this.xPos-unit<0){
+            this.xPos = 0;
+        }
+        else{this.xPos-=unit;}
+    }
+
+    moveDown(unit){
+        if(this.yPos+this.size+unit>canvasHeight){
+            this.yPos = canvasHeight-this.size;
+        }
+        else{this.yPos+=unit;}
+    
+    }
+
+    moveUp(unit){
+        if(this.yPos-unit<0){
+            this.yPos = 0;
+        }
+        else{this.yPos-=unit;}
+    }
 }
 
 class Food{
@@ -31,15 +64,17 @@ class Food{
     }
 }
 
-let snakes = [];
+let snakes = [];  
 
-let snake = new Snake(0,0,20)
+for(let i =0; i<2;i++){                                // Initilalize snakes here
+    let snake = new Snake(600-i*20,50,20,'right');
+    snakes.push(snake);
+}
 
-snakes.push(snake);
-
+//**** Adjust canvas and snake properties below! ****//
 canvas.width = 1500;
 
-canvas.style.backgroundColor = "white";
+canvas.style.backgroundColor = "black";
 
 let canvasColor = canvas.style.backgroundColor;
 
@@ -49,82 +84,91 @@ let canvasHeight = canvas.height;
 
 const ctx = canvas.getContext("2d");
 
-let playerColor = 'blue';
+let snakeColor = '#00FF00';
 
-let speed = 1;
+let snakeSize = 20;
 
-ctx.fillStyle = playerColor;
+let speed = snakeSize;  // Speed is the distance the snake moves in each frame. Measured in pixels.
 
-ctx.fillRect(snake.xPos,snake.yPos,snake.size,snake.size);
+ctx.fillStyle = snakeColor;
 
-function moveRight(unit){
-    if(snake.xPos+snake.size+unit>canvasWidth){
-        snake.xPos = canvasWidth-snake.size;
+let mainDirection = 'right'; // Main direction is the direction the head of snake is moving. 
+
+let snakeIsMoving = true;
+
+//*************************************************//
+
+
+function turnLoop(i,dir){
+    snakes[i].direction = dir;
+    if(i<snakes.length){
+        setTimeout(turnLoop,30, i+1 ,dir)}
     }
-    else{snake.xPos+=unit;}
-    snake.draw(ctx);
-}
-
-function moveLeft(unit){
-    if(snake.xPos-unit<0){
-        snake.xPos = 0;
-    }
-    else{snake.xPos-=unit;}
-    snake.draw(ctx);
-}
-
-function moveDown(unit){
-    if(snake.yPos+snake.size+unit>canvasHeight){
-        snake.yPos = canvasHeight-snake.size;
-    }
-    else{snake.yPos+=unit;}
-    snake.draw(ctx);
-
-}
-
-function moveUp(unit){
-    if(snake.yPos-unit<0){
-        snake.yPos = 0;
-    }
-    else{snake.yPos-=unit;}
-    snake.draw(ctx);
-}
-
-let direction = 'right'; 
 
 window.addEventListener('keydown',function(e){
-    console.log(e.key);
-    if(e.key==="ArrowRight"){
-        direction = 'right';
+    if(e.key==="ArrowRight" && mainDirection!=='left'){
+        mainDirection = 'right';
     }
-    else if(e.key==="ArrowLeft"){
-        direction = 'left';
+    else if(e.key==="ArrowLeft" && mainDirection!=='right'){
+        mainDirection = 'left';
     }
-    else if(e.key==="ArrowDown"){
-        direction = 'down';
+    else if(e.key==="ArrowDown" && mainDirection!=='up'){
+        mainDirection = 'down';
     }
-    else if(e.key==="ArrowUp"){
-        direction = 'up';
-    }
+    else if(e.key==="ArrowUp" && mainDirection!=='down'){
+        mainDirection = 'up';
+        }
+    turnLoop(0,mainDirection);
 })
 
-function gameLoop(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    snake.draw(ctx);
-    if(direction ==='right'){
-        moveRight(speed)
+function hitBorder(){
+    if(snakes[0].xPos ===0 || snakes[0].yPos ===0 || snakes[0].yPos === canvasHeight-snakeSize || snakes[0].xPos === canvasWidth-snakeSize){
+        return true;
     }
-    else if(direction==='left'){
-        moveLeft(speed)
-    }
-    else if(direction==='up'){
-        moveUp(speed)
-    }
-    else if(direction==='down'){
-        moveDown(speed)
-    }
-    setTimeout(gameLoop,1);
 }
+
+function intersect(){
+    console.log("placeholder")
+}
+
+
+
+
+
+
+
+
+function gameLoop(){  // Main game loop
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if(hitBorder()){
+        snakeIsMoving = false;
+    }
+    
+    for(let snake of snakes){
+        if(snakeIsMoving){
+            if(snake.direction ==='right'){
+                snake.moveRight(speed)
+            }
+            else if(snake.direction==='left'){
+                snake.moveLeft(speed)
+            }
+            else if(snake.direction==='up'){
+                snake.moveUp(speed)
+            }
+            else if(snake.direction==='down'){
+                snake.moveDown(speed)
+            }
+        }
+        snake.draw(ctx)
+            
+    }
+
+
+    setTimeout(gameLoop,30);
+}
+
 
 gameLoop();
 
